d7add8e7c96824bc1ad5eed9a2be19e3fe177a96
Bryan <balegria.gonz@gmail.com>
2022-04-28 15:11:55 -0500

    Add icons with Material-UI

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
+
+    const [isClicked, setIsClicked] = useState(false);
+
+    function inputClicked() {
+        setIsClicked(true);
+    };
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" onChange={props.addNote} value={props.titleValue}/>
-        <textarea name="content" placeholder="Take a note..." rows="3" onChange={props.addNote} value={props.textAreaValue}/>
-        <button onClick={props.clickHandler}>Add</button>
+      <form className="create-note">
+        {isClicked && <input name="title" placeholder="Title" onChange={props.addNote} value={props.titleValue}/>}
+        <textarea name="content" placeholder="Take a note..." rows={isClicked ? "3" : "1"} onChange={props.addNote} value={props.textAreaValue} onClick={inputClicked}/>
+        { isClicked && <div>
+            <Zoom in={true}>
+                <Fab onClick={props.clickHandler}><AddIcon /></Fab>
+            </Zoom>
+        </div>}
       </form>
     </div>
   );
